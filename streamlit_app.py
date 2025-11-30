aa69e77aa9676cb024a5440a1970d64651f32854
Jhonne <jonee4339@hotmail.com>
2025-11-30 09:56:50 -0300

    uplaod finally working

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,15 @@
+import os
+import shutil
+from langchain_openai import ChatOpenAI
 from pinecone import Pinecone, ServerlessSpec
 import streamlit as st
 from dotenv import load_dotenv, find_dotenv
 from pathlib import Path
+import tempfile
+from langchain_classic.chains.conversational_retrieval.base import (
+    ConversationalRetrievalChain,
+)
+from langchain_classic.memory import ConversationBufferMemory
 
 
 load_dotenv(find_dotenv(), override=True)
@@ -9,26 +17,27 @@ load_dotenv(find_dotenv(), override=True)
 pc = Pinecone()
 
 
+# Doc loading
 def load_document(file):
-    extension = st.write(Path(file.name).suffix)
-    print(extension)
+    extension = Path(file).suffix
     if extension == ".pdf":
-        from langchain_classic.document_loaders import PyPDFLoader
+        from langchain_community.document_loaders import PyPDFLoader
 
-        print(f"Loading {file}")
+        st.write(f"Loading {file}")
         loader = PyPDFLoader(file)
     elif extension == ".docx":
         from langchain_classic.document_loaders import Docx2txtLoader
 
+        st.write(f"Loading {file}")
         loader = Docx2txtLoader(file)
     else:
-        print("Document format is not supported")
+        st.write("Document format is not supported")
         return None
-
-    data = loader.load()
+    data = loader.load_and_split()
     return data
 
 
+# Chunking
 def chunk_data(data, chunk_size=256):
     from langchain_classic.text_splitter import RecursiveCharacterTextSplitter
 
@@ -39,6 +48,9 @@ def chunk_data(data, chunk_size=256):
     return chunks
 
 
+# Embedding cost
+
+
 def print_embedding_cost(texts):
     import tiktoken
 
@@ -46,6 +58,9 @@ def print_embedding_cost(texts):
     total_tokens = sum([len(enc.encode(page.page_content)) for page in texts])
     print(f"Total Tokens: {total_tokens}")
     print(f"Embedding Cost in USD: {total_tokens / 1000 * 0.00002:.6f}")
+
+
+# Create or load vector db
 
 
 def insert_or_fetch_embeddings(index_name, chunks):
@@ -77,25 +92,42 @@ def insert_or_fetch_embeddings(index_name, chunks):
         return vector_store
 
 
-# data = load_document("pdfs/guia_lgpd.pdf")
-# print(data[1].page_content)
-
-# print(f"You have {len(data)} pages in your data")
-
-# chunks = chunk_data(data)
-
 index_name = "askadocument"
 # vector_store = insert_or_fetch_embeddings(index_name, chunks)
 
 
+# Conversation history
+
+# llm = ChatOpenAI(model="gpt-5", temperature=0)
+# retriever = vector_store.as_retriever(search_type="similarity", search_kwards={"k": 5})
+# memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
+# crc = ConversationalRetrievalChain.from_llm(
+#     llm=llm, retriever=retriever, memory=memory, chain_type="stuff", verbose=True
+# )
+
+
+# data = load_document("pdfs/guia_lgpd.pdf")
+# print(data[1].page_content)
+
+
 # --------Streamlit---------
 
-st.subheader("PDF RAG Demo")
+working_dir = os.getcwd()
+
+st.subheader("Document RAG Demo")
+
 file = st.file_uploader("Upload PDF or DOCX ", type=["pdf", "docx"])
+
 if file is not None:
-    rag_file = load_document(file)
-    string_data = rag_file.read()
-    st.write(string_data)
-    st.text_area(f"Your file has {len(rag_file)} pages")
-# else:
-#     st.text_area("Unsupported format")
+    temp_file = "./temp.pdf"
+    file_path = f"{working_dir}/{file.name}"
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmpfile:
+        shutil.copyfileobj(file, tmpfile)
+        file_path = tmpfile.name
+
+    data = load_document(file_path)
+    st.write(f"Your file has {len(data)} pages")
+    chunks = chunk_data(data)
+    embed_cost = print_embedding_cost(chunks)
+    st.write(f"This document will cost {embed_cost} to embed")
